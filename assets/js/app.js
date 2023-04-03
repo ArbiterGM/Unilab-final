e68911367ec093688e73e9e866622c11da5b9191
ArbiterGM <123399265+ArbiterGM@users.noreply.github.com>
2023-04-03 17:19:01 +0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,25 @@
-for(i = 1; i < 18; i++) {
-    const modal = document.querySelector('.modal' + [i]);
+// hamburger menu
+const hamburger = document.getElementById("hamburger");
+const header = document.getElementById("header2");
 
-    const modalOpener = document.querySelector('.modal-opener' + [i])
-    
-    const modalCloser = document.querySelector('.modal-closer' + [i])
+hamburger.addEventListener("click", () => {
+  header.classList.toggle("header2-active");
+});
 
-    // open
-    modalOpener.onclick = function() {
-        modal.style.display = "block";
-    }
+// modals
+for (i = 1; i < 18; i++) {
+  const modal = document.querySelector(".modal" + [i]);
 
-    // close
-    modalCloser.onclick = function() {
-        modal.style.display = "none";
-    }
+  const modalOpener = document.querySelector(".modal-opener" + [i]);
+
+  const modalCloser = document.querySelector(".modal-closer" + [i]);
+  // open
+  modalOpener.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  // close
+  modalCloser.onclick = function () {
+    modal.style.display = "none";
+  };
 }
