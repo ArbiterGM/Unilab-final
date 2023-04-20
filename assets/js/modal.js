ca37c43c5e422a46043b1e4791eaa79a5365c79c
ArbiterGM <123399265+ArbiterGM@users.noreply.github.com>
2023-04-20 12:10:23 +0400

    Add files via upload

---
+++ assets/js/modal.js
@@ -0,0 +1,17 @@
+// modals
+for (i = 1; i < 18; i++) {
+    const modal = document.querySelector(".modal" + [i]);
+  
+    const modalOpener = document.querySelector(".modal-opener" + [i]);
+  
+    const modalCloser = document.querySelector(".modal-closer" + [i]);
+    // open
+    modalOpener.addEventListener("click", function () {
+      modal.style.display = "block";
+    });
+  
+    // close
+    modalCloser.onclick = function () {
+      modal.style.display = "none";
+    };
+  }
